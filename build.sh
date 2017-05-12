d0fd9739b2c776a255c95e67ada77b6ab89cf980
Matthew Batchelder <borkweb@gmail.com>
2017-05-12 11:22:41 -0400

    Add build script

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker build -t acmemud/mudlib:1.0 build/mudlib
+docker build -t acmemud/driver:3.5.0 build/driver
+docker build -t acmemud/mud:3.5.0 build/mud
--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-docker build -t acmemud/mudlib:1.0 build/mudlib
-docker build -t acmemud/driver:3.5.0 build/driver
-docker build -t acmemud/mud:3.5.0 build/mud
 docker-compose up -d
